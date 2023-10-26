f8f84361bac5881e4d2fde30433fdfcc788bd8db
Trong Nguyen <nguyentrong101094@gmail.com>
2023-10-26 11:16:14 +0700

    improve testing

--- Assets/AdmobTest.cs
+++ Assets/AdmobTest.cs
@@ -10,6 +10,8 @@ public class AdmobTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
+        
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize((InitializationStatus initStatus) =>
         {
