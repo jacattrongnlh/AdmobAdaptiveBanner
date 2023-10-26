8f3f6524d4d0dd5a064078931d4688ec2c9e30a9
Trong Nguyen <nguyentrong101094@gmail.com>
2023-10-26 13:46:31 +0700

    add console

--- Assets/AdmobTest.cs
+++ Assets/AdmobTest.cs
@@ -11,12 +11,13 @@ public class AdmobTest : MonoBehaviour
     void Start()
     {
         Application.targetFrameRate = 60;
-        
+
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize((InitializationStatus initStatus) =>
         {
             // This callback is called once the MobileAds SDK is initialized.
             // LoadAd();
+            DebugConsole.Log(initStatus.ToString());
         });
     }
 
@@ -27,13 +28,14 @@ public class AdmobTest : MonoBehaviour
     /// </summary>
     public void CreateBannerView(AdSize adSize, AdPosition adPosition)
     {
-        Debug.Log("Creating banner view");
+        DebugConsole.Log("Creating banner view");
 
         // If we already have a banner, destroy the old one.
         if (_bannerView != null) { DestroyBannerView(); }
 
         // Create a 320x50 banner at top of the screen
         _bannerView = new BannerView(_adUnitId, adSize, adPosition);
+        ListenToAdEvents();
     }
 
     /// <summary>
@@ -42,7 +44,10 @@ public class AdmobTest : MonoBehaviour
     public void LoadDefaultBannerAd()
     {
         // create an instance of a banner view first.
-        if (_bannerView == null) { CreateBannerView(AdSize.Banner, AdPosition.Top); }
+        // if (_bannerView == null) 
+        {
+            CreateBannerView(AdSize.Banner, AdPosition.Top);
+        }
 
         // create our request used to load the ad.
         var adRequest = new AdRequest();
@@ -56,7 +61,10 @@ public class AdmobTest : MonoBehaviour
     {
         // create an instance of a banner view first.
         var adSize = AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
-        if (_bannerView == null) { CreateBannerView(adSize, AdPosition.Top); }
+        // if (_bannerView == null) 
+        {
+            CreateBannerView(adSize, AdPosition.Top);
+        }
 
         // create our request used to load the ad.
         var adRequest = new AdRequest();
@@ -73,9 +81,43 @@ public class AdmobTest : MonoBehaviour
     {
         if (_bannerView != null)
         {
-            Debug.Log("Destroying banner view.");
+            DebugConsole.Log("Destroying banner view.");
             _bannerView.Destroy();
             _bannerView = null;
         }
+    }
+
+    /// <summary>
+    /// listen to events the banner view may raise.
+    /// </summary>
+    private void ListenToAdEvents()
+    {
+        // Raised when an ad is loaded into the banner view.
+        _bannerView.OnBannerAdLoaded += () =>
+        {
+            DebugConsole.Log("Banner view loaded an ad with response : "
+                             + _bannerView.GetResponseInfo());
+        };
+        // Raised when an ad fails to load into the banner view.
+        _bannerView.OnBannerAdLoadFailed += (LoadAdError error) =>
+        {
+            DebugConsole.Log("Banner view failed to load an ad with error : "
+                             + error);
+        };
+        // Raised when the ad is estimated to have earned money.
+        _bannerView.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log(string.Format("Banner view paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
+        };
+        // Raised when an impression is recorded for an ad.
+        _bannerView.OnAdImpressionRecorded += () => { Debug.Log("Banner view recorded an impression."); };
+        // Raised when a click is recorded for an ad.
+        _bannerView.OnAdClicked += () => { Debug.Log("Banner view was clicked."); };
+        // Raised when an ad opened full screen content.
+        _bannerView.OnAdFullScreenContentOpened += () => { Debug.Log("Banner view full screen content opened."); };
+        // Raised when the ad closed full screen content.
+        _bannerView.OnAdFullScreenContentClosed += () => { Debug.Log("Banner view full screen content closed."); };
     }
 }
---
+++ Assets/DebugConsole.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugConsole : MonoBehaviour
+{
+    [SerializeField] private Text debugText;
+
+    private void Start() { OnLogText += AddText; }
+
+    private void AddText(string obj) { debugText.text += $"{obj}\n"; }
+
+    public static Action<string> OnLogText;
+
+    public static void Log(string text)
+    {
+        OnLogText?.Invoke(text);
+    }
+}
